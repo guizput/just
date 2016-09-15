6ccec945d79be8c18c289e0423b4fd5003efd29c
guizput <guillaumeduran2@gmail.com>
2016-09-16 00:44:40 +0200

    Cleaning up the js

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
-function hasClass(el, cls) {
-  return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
-}
-
 var nav = document.querySelector('#menu'),
 		navToggle = document.querySelectorAll('.nav-toggle'),
 		menuElement = document.querySelectorAll('#menu ul li');
 
-navToggle[0].addEventListener('click', function(e){
+function hasClass(el, cls) {
+  return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
+}
+
+function activeToggle (){
 
 	if(hasClass(nav, 'active')){
 
@@ -20,23 +20,20 @@ navToggle[0].addEventListener('click', function(e){
 
 	}
 
+}
+
+
+navToggle[0].addEventListener('click', function(e){
+
+	activeToggle();
+
 });
 
 for(i=0; i < menuElement.length; i++){
 
 	menuElement[i].addEventListener('click', function(e){
 
-		if(hasClass(nav, 'active')){
-
-			nav.className = '';
-			navToggle[0].className = 'nav-toggle';
-
-		}else{
-
-			nav.className = 'active';
-			navToggle[0].className = 'nav-toggle open';
-
-		}
+		activeToggle();
 
 	});
 
