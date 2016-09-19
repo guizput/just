849e8abbb70944eefd0396671d1df3fa3961cb06
Guillaume Duran <guillaume.duran@garmin.com>
2016-09-19 16:04:37 +0200

    Calling a specific Playlist instead of all videos of channel

--- src/php/list_youtube_videos.php
+++ src/php/list_youtube_videos.php
@@ -1,22 +1,24 @@
 <?php
 
 $API_key = 'AIzaSyAGnvXLx0dyqNlWzLmbI9hPnPmu8uXn2vo';
+$playlistId = 'PL8T_kI1HrvoHE7QpyMKfWLd5_B15SYw6c';
 $channelId = 'UC779-tEQQ8FWG3aA5oTWYCA';
 $maxResults = 9;
 
 
-$video_list = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$channelId.'&maxResults='.$maxResults.'&key='.$API_key.''));
+$video_list = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId='.$playlistId.'&maxResults='.$maxResults.'&key='.$API_key));
 
+// https://www.youtube.com/playlist?list=PL8T_kI1HrvoHE7QpyMKfWLd5_B15SYw6c
 
 if($video_list){
   // Stock data in variables
   foreach($video_list->items as $item){
     //Embed video
-    if(isset($item->id->videoId)){
+    if(isset($item->snippet->resourceId->videoId) && isset($item->snippet->title)){
         
 			echo '
-				<div class="video-thumbnail active" id="'.$item->id->videoId.'">
-				  <img src="https://i.ytimg.com/vi/'.$item->id->videoId.'/mqdefault.jpg">
+				<div class="video-thumbnail active" id="'.$item->snippet->resourceId->videoId.'">
+				  <img src="https://i.ytimg.com/vi/'.$item->snippet->resourceId->videoId.'/mqdefault.jpg">
 				  <div class="video-title">'.$item->snippet->title.'</div>
 				</div>
 			';
