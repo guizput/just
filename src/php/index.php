977055de61597499175fd5943a8ccc9b7c8c71f6
guizput <guillaumeduran2@gmail.com>
2016-10-18 21:59:53 +0200

    Added php extension to templates

--- src/php/index.php
+++ src/php/index.php
@@ -1,15 +1,15 @@
 <?php
 // Include MobileDetect and instantiate the class.
-require_once dirname(__FILE__) . '/php/Mobile_Detect.php';
+require_once 'php/Mobile_Detect.php';
 $detect = new Mobile_Detect;
 
 // Any mobile device (phones or tablets).
-// if ( $detect->isMobile() ) {
-// 	include('/templates/mobile.html');
-// }else if( $detect->isTablet() ){
-// 	include('/templates/tablet.html');
-// }else {
-// 	include('/templates/desktop.html');
-// }
+if ( $detect->isMobile() ) {
+	include('templates/mobile.php');
+}else if( $detect->isTablet() ){
+	include('templates/tablet.php');
+}else {
+	include('templates/desktop.php');
+}
 
 ?>
